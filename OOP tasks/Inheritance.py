e89eb7db6e384448c13a8cd28e5a63121fd61d96
rokitapiotr <rokitapiotr97@gmail.com>
2023-10-19 21:10:12 +0200

    OOP - Python

--- OOP tasks/Inheritance.py
+++ OOP tasks/Inheritance.py
@@ -4,23 +4,22 @@ Inheritance. Create several classes that demonstrate the inheritance. Add the co
 
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name):
         self.name = name
-        self.age = age
 
     def introduce_yourself(self):
         print("Hello!")
 
 
 class Employee(Person):
-    def __init__(self, name, age, job_title):
-        super().__init__(name, age)
+    def __init__(self, name, job_title):
+        super().__init__(name)
         self.job_title = job_title
 
     def introduce_yourself(self):
         print("Hello! My name is {} and I am a {}.".format(self.name, self.job_title))
 
 
-employee = Employee("Piotr Rokita", 12, "Software Engineer")
+employee = Employee("Piotr Rokita", "Software Engineer")
 
 employee.introduce_yourself()
